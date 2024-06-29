c916a789855d3123bd9523c0f240944cabd22560
alikemalcoskun <alikemalcoskun0@gmail.com>
2024-06-29 16:11:08 +0300

    Update WebSecurityConfig to add db connection to Auth

--- src/main/java/com/alikemal/flightbooking/security/config/WebSecurityConfig.java
+++ src/main/java/com/alikemal/flightbooking/security/config/WebSecurityConfig.java
@@ -1,86 +1,49 @@
 package com.alikemal.flightbooking.security.config;
 
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.Properties;
+import javax.sql.DataSource;
 
-@EnableWebSecurity
 @Configuration
-@AllArgsConstructor
 public class WebSecurityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails user = org.springframework.security.core.userdetails.User
-                .withUsername("user")
-                .password(passwordEncoder().encode("password"))
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.setUsersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username = ?");
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("SELECT username, role FROM users WHERE username = ?");
+        return jdbcUserDetailsManager;
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // TODO: hasRole("ADMIN") is not working properly, fix it
         http
-                .csrf(AbstractHttpConfigurer::disable)
-
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/users/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/airports/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, "/airports/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/aircraft/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, "/aircraft/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/flights/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, "/flights/**").hasRole("ADMIN")
-                        .anyRequest().permitAll()
-                )
-
-                .formLogin(formLogin -> formLogin
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .logout(LogoutConfigurer::permitAll);
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers(HttpMethod.POST, "/api/v1/registration/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/v1/flights/search").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/v1/airports/**").permitAll()
+                                .anyRequest().authenticated()
+                );
+        http.httpBasic(Customizer.withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
-
-    @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("localhost");
-        mailSender.setPort(1025);
-        mailSender.setUsername("username");
-        mailSender.setPassword("password");
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
-
-        return mailSender;
-    }
-
 }
